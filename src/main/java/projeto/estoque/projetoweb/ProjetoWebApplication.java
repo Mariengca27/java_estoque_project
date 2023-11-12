4c91445bb59b4b69d7b474a4040f4da860d87b59
Mariana Machado Amaral <mariengca27@gmail.com>
2023-11-12 18:40:49 -0300

    Primeiro projeto estoque - Testando a classe e o métodos principal

---
+++ src/main/java/projeto/estoque/projetoweb/ProjetoWebApplication.java
@@ -0,0 +1,22 @@
+package projeto.estoque.projetoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebApplication {
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebApplication.class, args);
+
+
+
+		System.out.println("Testando a clase e método principal");
+	}
+
+
+
+}
---
+++ src/test/java/projeto/estoque/projetoweb/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.estoque.projetoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
